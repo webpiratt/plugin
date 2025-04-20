f91ff85e738425106787d16b23e1453b8b6db744
Raghav Sood <r@raghavsood.com>
2025-04-21 01:23:52 +0800

    walletcore: remove

--- walletcore/core/core_linux.go
+++
@@ -1,7 +0,0 @@
-//go:build linux
-
-package core
-
-// #cgo CFLAGS: -I../include
-// #cgo LDFLAGS: -L../libs/linux -lTrustWalletCore -lwallet_core_rs -lprotobuf -lTrezorCrypto -lstdc++ -lm
-import "C"
